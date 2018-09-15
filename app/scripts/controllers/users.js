f03421437c2fe4950245b2b428fdbd633fd5808b
angiealejo11 <angie.alejo.cdc@gmail.com>
2018-09-14 21:50:30 -0500

    Initial commit

---
+++ app/scripts/controllers/users.js
@@ -0,0 +1,14 @@
+/**
+ * Función para agregar toda la logica de la administración 
+ * usuarios
+ */
+
+function usersActionsController() {
+    //tu código aquí.  
+}
+
+funcion guardar(){
+var nombre=document.getElementsById ("nombre").value;
+var profesion=
+
+}
